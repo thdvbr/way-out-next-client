9704e0ee3ea9acaad08db3e3f9202683edadebb3
Soyoon Choi <soyoon.choi@code.berlin>
2021-05-09 11:43:08 +0200

    get props on home page and display hero and masonry grid

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,48 @@
 import React from 'react';
+import Head from 'next/head';
+import Container from '../components/container';
+import HeroPost from '../components/hero-post';
+import { Toolbar } from '../components/toolbar';
+import { getClient, overlayDrafts } from '../utils/sanity.server';
+import { indexQuery } from '../utils/queries';
+import MasonryGrid from '../components/masonry-grid';
+import Layout from '../components/layout';
 
-export default function Home() {
-  return <div>hello world</div>;
-}
+export const Index = ({ allPosts, preview }) => {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Way Out Mag</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Container>
+          <Toolbar />
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              subtitle={heroPost.subtitle}
+              mainImage={heroPost.mainImage}
+              slug={heroPost.slug}
+            />
+          )}
+          {morePosts && <MasonryGrid posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+};
+
+export const getStaticProps = async ({ preview = false }) => {
+  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery));
+  return {
+    props: { allPosts, preview },
+  };
+};
+
+export default Index;
