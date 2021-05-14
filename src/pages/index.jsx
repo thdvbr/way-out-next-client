a9c1beee1957634b734fc61c6f09e960d5d70a5c
Soyoon Choi <soyoon.choi@code.berlin>
2021-05-14 17:19:30 +0200

    add breakpoint to display all posts on preview block when mobile

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,37 +1,51 @@
 import React from 'react';
 import Head from 'next/head';
+import { Breakpoint, BreakpointProvider } from 'react-socks';
 import { getClient, overlayDrafts } from '../utils/sanity.server';
 import { indexQuery } from '../utils/queries';
 import {
-  Container, HeroPost, Toolbar, PreviewGrid, Layout,
+  Container,
+  HeroPost,
+  Toolbar,
+  PreviewGrid,
+  Layout,
 } from '../components';
+
 
 export const Index = ({ allPosts, preview }) => {
   const heroPost = allPosts[0];
   const morePosts = allPosts.slice(1);
   return (
     <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Way Out Mag</title>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <Container>
-          <Toolbar />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              subtitle={heroPost.subtitle}
-              mainImage={heroPost.mainImage}
-              slug={heroPost.slug}
+      <BreakpointProvider>
+        <Layout preview={preview}>
+          <Head>
+            <title>Way Out Mag</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
             />
-          )}
-          {morePosts && <PreviewGrid posts={morePosts} />}
-        </Container>
-      </Layout>
+          </Head>
+          <Container>
+            <Toolbar />
+            <Breakpoint customQuery="(max-width: 575px)">
+              <PreviewGrid posts={allPosts} />
+            </Breakpoint>
+            <Breakpoint customQuery="(min-width: 576px)">
+              {heroPost && (
+                <HeroPost
+                  title={heroPost.title}
+                  subtitle={heroPost.subtitle}
+                  mainImage={heroPost.mainImage}
+                  mainCategory={heroPost.mainCategory}
+                  slug={heroPost.slug}
+                />
+              )}
+              {morePosts && <PreviewGrid posts={morePosts} />}
+            </Breakpoint>
+          </Container>
+        </Layout>
+      </BreakpointProvider>
     </>
   );
 };
