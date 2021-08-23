d1ef5a13586cc4f668c738d3517e3ae9c8278af3
Soyoon Choi <soyoon.choi@code.berlin>
2021-08-24 00:39:13 +0200

    create footer with svg background

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
-import Container from './container';
+import Link from 'next/link';
+import FooterSvg from './footer-svg';
+import { renderToStaticMarkup } from "react-dom/server";
+import { useAppContext } from '../context/state';
 
 export default function Footer() {
+  const {
+    infoIsOpen, setInfoIsOpen,
+  } = useAppContext();
+  const svgString = encodeURIComponent(renderToStaticMarkup(<FooterSvg />));
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
+    <footer>
+      <nav
+        className="flex flex-row text-white py-5 xl:text-15 text-12 font-title"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,${svgString}")`,
+        }}>
+        <div className="w-1/3 flex flex-row justify-evenly items-center underline">
+          <Link href="/interviews">
+            <a href="/interviews">
+              Interviews
             </a>
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/example"
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
+          </Link>
+          <Link href="/stuff-we-like">
+            <a href="/stuff-we-like">
+              <span className="">Stuff We Like</span>
             </a>
-          </div>
+          </Link>
+          <Link href="/">
+            <a href="/radio">
+              <span>Radio</span>
+            </a>
+          </Link>
+          <button
+            type="button"
+            onClick={() => setInfoIsOpen(!infoIsOpen)}>
+            <span>Info</span>
+          </button>
         </div>
-      </Container>
+        <div className="w-1/3">join our newsletter</div>
+        <div className="w-1/3">logo</div>
+      </nav>
     </footer>
   );
 }
