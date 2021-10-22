a55d9225f89e12a1ffed3fdc97a487fc03f7df87
Soyoon Choi <soyoon.choi@code.berlin>
2021-10-22 16:43:24 +0200

    navbar space evenly

--- src/components/navbar-desktop.jsx
+++ src/components/navbar-desktop.jsx
@@ -7,32 +7,56 @@ import SearchBar from './search-bar';
 
 const NavbarDesktop = () => {
   const {
-    handleSearch, handleSearchOpen, searchIsOpen, infoIsOpen, setInfoIsOpen,
+    handleSearch,
+    handleSearchOpen,
+    searchIsOpen,
+    infoIsOpen,
+    setInfoIsOpen,
   } = useAppContext();
 
   return (
     <div className="sticky top-0 font-title sm:text-15 lg:text-17 xl:text-22.5">
-      <nav className="py-3 flex relative flex-wrap justify-between sm:w-full">
-        <button type="button" onClick={handleSearchOpen} className="sm:w-1/5 search">
-          <span className="sm:float-left"><FiSearch className="search-icon" /></span>
+      <nav className="py-3 flex relative justify-between sm:w-full">
+        <button
+          type="button"
+          onClick={handleSearchOpen}
+          className="search sm:float-left inline-block">
+          <span className="sm:float-left">
+            <FiSearch className="search-icon" />
+          </span>
         </button>
-        <Link href="/interviews">
-          <a href="/interviews" className="sm:flex-auto sm:py-0 interviews w-1/5">
-            Interviews
-          </a>
-        </Link>
-        <Link href="/stuff-we-like">
-          <a href="/stuff-we-like" className="sm:flex-auto sm:py-0 stuff-we-like w-1/5">
-            <span className="flex justify-center">Stuff We Like</span>
-          </a>
-        </Link>
-        <Link href="/">
-          <a href="/radio" className="sm:flex-auto sm:py-0 radio w-1/5">
-            <span className="sm:float-right">Radio</span>
-          </a>
-        </Link>
-        <button type="button" onClick={() => setInfoIsOpen(!infoIsOpen)} className="sm:flex-auto sm:py-0 info w-1/5">
-          <span className="sm:float-right">Info</span>
+        <div>
+          <Link href="/interviews">
+            <a
+              href="/interviews"
+              className="sm:float-left inline-block sm:py-0 interviews inline-block">
+              Interviews
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/stuff-we-like">
+            <a
+              href="/stuff-we-like"
+              className="sm:py-0 stuff-we-like sm:float-left">
+              Stuff We Like
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href="/">
+            <a
+              href="/radio"
+              className="sm:py-0 radio sm:float-left inline-block">
+              <span className="sm:float-left">Radio</span>
+            </a>
+          </Link>
+        </div>
+        <button
+          type="button"
+          onClick={() => setInfoIsOpen(!infoIsOpen)}
+          className="sm:py-0 info sm:float-left inline-block">
+          <span className="sm:float-left">Info</span>
         </button>
       </nav>
       <motion.div className={`${searchIsOpen ? 'flex' : 'hidden'} w-full`}>
