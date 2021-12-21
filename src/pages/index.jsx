2f2e51f3a70ff9619413d4dfce55bf12aa6faf14
thdvbr <soyoon.choi@code.berlin>
2021-12-21 16:01:09 +0100

    fix breakpoint

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,7 +30,7 @@ export const Index = ({ allPosts, preview }) => {
     <>
       <Layout preview={preview}>
         <Container>
-          <Breakpoint customQuery="(min-width: 500px)">
+          <Breakpoint customQuery="(max-width: 500px)">
             <div className="px-3">
               <MasonryGrid posts={!query ? allPosts : searchResult} />
             </div>
