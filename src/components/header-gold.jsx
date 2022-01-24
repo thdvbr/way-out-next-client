2debc8f4f45e3db1bc90571ca80168686caedd32
thdvbr <soyoon.choi@code.berlin>
2022-01-24 22:50:25 +0100

    change header gold file to png

--- src/components/header-gold.jsx
+++ src/components/header-gold.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import Link from 'next/link';
 import { motion, useViewportScroll, useTransform } from 'framer-motion';
-import Logo from './logo-svg';
-import LogoGold from './logo-gold-svg';
+import { useAppContext } from '../context/state';
 
 export default function HeaderGold() {
   const { scrollYProgress } = useViewportScroll();
+  const { joinIsOpen, setJoinIsOpen , setInfoIsOpen} = useAppContext();
   // returns where Y is in a range, from 0 to 1
   // second array [0,1] = where our start and end of scroll is
-  const opacity = useTransform(scrollYProgress, [0, 0.03], [0, 1]);
-  const scale = useTransform(scrollYProgress, [0, 0.5], [1, 1.5]);
+  const opacity = useTransform(scrollYProgress, [-1, 0.03], [0, 1]);
   return (
     <header>
-      <div className="flex relative flex-wrap justify-between pt-16 mb-40 font-main xl:text-22.5 lg:text-17 md:text-17">
-        <span>Donate</span>
-        <span>Join</span>
+      <div className="flex relative -top-10 flex-wrap justify-between pt-16 mb-14 font-main xl:text-22.5 lg:text-17 md:text-17">
+        <span className="z-50 button-underline">Donate</span>
+        <button
+          type="button"
+          onClick={() => { setJoinIsOpen(!joinIsOpen); setInfoIsOpen(false) }}
+          className="z-50 button-underline">
+          Join
+        </button>
         <Link href="/">
-          <motion.a
+          <a
             href="/"
-            whileHover={{ scale: 1.1, dropShadow: '0px 0px 4px gray' }}
-            className="flex absolute left-0 right-0 justify-center z-40">
-            <Logo />
-          </motion.a>
+            className="flex absolute left-0 right-0 top-4 sm:top-6 lg:top-5 xl:top-3 justify-center z-40">
+            {/* <Logo /> */}
+            <div className="logo-container sticky top-0"/>
+            <div className="gif"/>
+          </a>
         </Link>
-        <motion.div
-          style={{ scale: scale, opacity: opacity }}
-          className="flex fixed left-0 right-0 justify-center">
-          <LogoGold />
-        </motion.div>
+{/* fixed positioning doesnt work if parent div uses translate */}
+        {/* <motion.div
+          style={{ opacity }}
+          className="fixed ">
+          {/* <LogoGold /> */}
+          {/* <div className="logo-gold-container" /> */}
+        {/* </motion.div> */} 
       </div>
     </header>
   );
