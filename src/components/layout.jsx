6b86ca6802d20465851b799ccde91171c1a04fb1
thdvbr <soyoon.choi@code.berlin>
2022-03-21 18:24:01 +0100

    fix mobile ad not displaying problem

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -62,63 +62,69 @@ export default function Layout({ preview, bottomAds, children }) {
           variants={infoVariants}
           animate={infoIsOpen ? 'opened' : 'closed'}
           transition={{ type: 'spring', duration: 1 }}> */}
-          <Head>
-            <title>Way Out Mag</title>
-            <meta
-              name="viewport"
-              content="initial-scale=1.0, width=device-width"
-            />
-          </Head>
-          <Breakpoint customQuery="(max-width: 499px)">
-            <div className="sticky top-0 z-30">
-              <Container>
-                <NavbarMobile />
-                <InfoDrawer />
-              </Container>
-            </div>
-            <main className="w-screen inset-0 z-0">{children}</main>
-          </Breakpoint>
-          {/* desktop */}
-          <Breakpoint customQuery="(min-width: 500px)">
+        <Head>
+          <title>Way Out Mag</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Breakpoint customQuery="(max-width: 499px)">
+          <div className="sticky top-0 z-30">
+            <Container>
+              <NavbarMobile />
+              <InfoDrawer />
+            </Container>
+          </div>
+          <main className="w-screen inset-0 z-0">{children}</main>
+          {randomSlice1 && (
             <motion.div
-              initial={false}
-              variants={joinVariants}
-              animate={joinIsOpen ? 'opened' : 'closed'}
-              transition={{ type: 'linear', duration: 0.3 }}>
-              <Subscribe />
-              <Container>
-                <div className="px-3">
-                  <Header />
-                  <SectionSeparator />
-                </div>
-                <InfoDrawer />
-                <NavbarDesktop />
-              </Container>
-              <main className="w-screen inset-0 z-0 -mt-3">{children}</main>
-              {randomSlice1 && (
-                <motion.div
-                  className="flex justify-center px-3 mb-16 md:px-8 ml:px-20 lg:px-28"
-                  ref={ref}
-                  animate={animation}
-                  variants={adVariants}
-                  initial="hidden">
-                  {width > 500 ? (
-                    <BottomAdImage
-                      image={randomSlice1.adImage}
-                      url={randomSlice1.adUrl}
-                      width={1360}
-                    />
-                  ) : (
-                    <BottomAdImage
-                      image={randomSlice1.adImageMobile}
-                      url={randomSlice1.adUrl}
-                      width={500}
-                    />
-                  )}
-                </motion.div>
-              )}
+              className="flex justify-center px-3 mb-16 md:px-8 ml:px-20 lg:px-28"
+              ref={ref}
+              animate={animation}
+              variants={adVariants}
+              initial="hidden">
+              <BottomAdImage
+                image={randomSlice1.adImageMobile}
+                url={randomSlice1.adUrl}
+                width={500}
+              />
             </motion.div>
-          </Breakpoint>
+          )}
+        </Breakpoint>
+        {/* desktop */}
+        <Breakpoint customQuery="(min-width: 500px)">
+          <motion.div
+            initial={false}
+            variants={joinVariants}
+            animate={joinIsOpen ? 'opened' : 'closed'}
+            transition={{ type: 'linear', duration: 0.3 }}>
+            <Subscribe />
+            <Container>
+              <div className="px-3">
+                <Header />
+                <SectionSeparator />
+              </div>
+              <InfoDrawer />
+              <NavbarDesktop />
+            </Container>
+            <main className="w-screen inset-0 z-0 -mt-3">{children}</main>
+            {randomSlice1 && (
+              <motion.div
+                className="flex justify-center px-3 mb-16 md:px-8 ml:px-20 lg:px-28"
+                ref={ref}
+                animate={animation}
+                variants={adVariants}
+                initial="hidden">
+                <BottomAdImage
+                  image={randomSlice1.adImage}
+                  url={randomSlice1.adUrl}
+                  width={1360}
+                />
+              </motion.div>
+            )}
+          </motion.div>
+        </Breakpoint>
         {/* </motion.div> */}
       </motion.div>
     </>
