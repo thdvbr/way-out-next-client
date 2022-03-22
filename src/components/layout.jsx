41d9d8951c18bba9c671dfd4c50e532175057228
thdvbr <soyoon.choi@code.berlin>
2022-03-22 23:40:57 +0100

    make subscribe and ad fixed position to the bottom when path is /search

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -4,8 +4,10 @@ import { useCurrentWidth, Breakpoint } from 'react-socks';
 import { motion, useAnimation } from 'framer-motion';
 import _ from 'lodash';
 import { useInView } from 'react-intersection-observer';
+import { useRouter } from 'next/router';
 import AlertPreview from './alert-preview';
 import { useAppContext } from '../context/state';
+
 
 // eslint-disable-next-line import/no-cycle
 import {
@@ -27,6 +29,7 @@ export default function Layout({ preview, bottomAds, children }) {
     closed: { x: 0 },
   };
   const { infoIsOpen, joinIsOpen } = useAppContext();
+  const { asPath, pathname } = useRouter();
 
   const randomSlice1 = _.sample(bottomAds);
   const { ref, inView } = useInView();
@@ -77,21 +80,23 @@ export default function Layout({ preview, bottomAds, children }) {
             </Container>
           </div>
           <main className="w-screen inset-0 z-0">{children}</main>
-          <Subscribe />
-          {randomSlice1 && (
-            <motion.div
-              className="flex justify-center px-3 mb-16 md:px-8 ml:px-20 lg:px-28"
-              ref={ref}
-              animate={animation}
-              variants={adVariants}
-              initial="hidden">
-              <BottomAdImage
-                image={randomSlice1.adImageMobile}
-                url={randomSlice1.adUrl}
-                width={500}
-              />
-            </motion.div>
-          )}
+          <div className={pathname === '/search' ? 'fixed bottom-0' : undefined}>
+            <Subscribe />
+            {randomSlice1 && (
+              <motion.div
+                className="flex justify-center px-3 mb-16 md:px-8 ml:px-20 lg:px-28"
+                ref={ref}
+                animate={animation}
+                variants={adVariants}
+                initial="hidden">
+                <BottomAdImage
+                  image={randomSlice1.adImageMobile}
+                  url={randomSlice1.adUrl}
+                  width={500}
+                />
+              </motion.div>
+            )}
+          </div>
         </Breakpoint>
         {/* desktop */}
         <Breakpoint customQuery="(min-width: 500px)">
