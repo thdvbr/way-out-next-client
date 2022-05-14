4b91131d477a613e668df6ad5689298060e636d6
thdvbr <soyoon.choi@code.berlin>
2022-05-14 21:04:06 +0200

    resize post title and subtitle

--- src/components/post-subtitle.jsx
+++ src/components/post-subtitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PostSubtitle({ children }) {
   return (
-    <h1 className="block font-subtitle text-20.5 leading-7 sm:text-27.5 sm:leading-8 text-center lg:text-43 xl:text-57 lg:leading-tight mb-9 xl:px-60 px-8 ml:px-42 md:px-16">
+    <h1 className="block font-subtitle text-20.5 leading-7 sm:text-27.5 sm:leading-8 text-center lg:text-43 xl:text-57 lg:leading-tight mb-9 xl:px-60 px-8 ml:px-48 md:px-44">
       {children}
     </h1>
   );
