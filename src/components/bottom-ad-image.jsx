176eb15a3c0e3904a04c1ec4812cbf663da09ba1
thdvbr <soyoon.choi@code.berlin>
2022-05-16 21:18:59 +0200

    use next/image on bottom ad

--- src/components/bottom-ad-image.jsx
+++ src/components/bottom-ad-image.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
+import { useNextSanityImage } from 'next-sanity-image';
 import { motion } from 'framer-motion';
-import imageUrlBuilder from '@sanity/image-url';
 import { sanityClient } from '../utils/sanity.server';
 
-const BottomAdImage = ({ image, url, width }) => {
-  const urlFor = (source) => {
-    return imageUrlBuilder(sanityClient).image(source);
+const BottomAdImage = ({ image: source, url }) => {
+  const myCustomImageBuilder = (imageUrlBuilder) => {
+    return imageUrlBuilder.width(1500);
   };
 
-  const adImage = image && <img src={urlFor(image).width(width).url()} />;
+  const imageProps = useNextSanityImage(sanityClient, source, {
+    blurUpImageWidth: 124,
+    blurUpImageQuality: 40,
+    blurUpAmount: 200,
+    imageBuilder: myCustomImageBuilder,
+  });
+
+  const image = source ? (
+    <Image src={imageProps.src} loader={imageProps.loader} alt='Bottom ad image' sizes="(min-width: 500px), 800px auto" layout="fill" objectFit="cover" />
+  ) : (
+    <div style={{ paddingTop: '50%', backgroundColor: '#ddd' }} />
+  );
+
   return (
     <motion.div
-      className="sm:px-3 xl:px-0"
+      style={{width: '100%', height:'200px', maxWidth:'1280px', position: 'relative'}}
       initial={{ x: -60, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
       transition={{ delay: 0.6 }}>
-      <a href={url}>{adImage}</a>
+      <a href={url}>{image}</a>
     </motion.div>
   );
 };
