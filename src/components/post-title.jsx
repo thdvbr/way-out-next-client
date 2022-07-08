24045e37ae69dc16eccfa8102fbc1ff33d82ea6b
thdvbr <soyoon.choi@code.berlin>
2022-07-08 16:08:18 +0200

    adjust line height post title and subtitle

--- src/components/post-title.jsx
+++ src/components/post-title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PostTitle({ children }) {
   return (
-    <h1 className="block font-title text-22.5 leading-8 sm:text-30 lg:text-42 xl:text-55.5 lg:leading-tight text-center mt-6 sm:mt-10 sm:leading-9 px-12 md:px-48 ml:px-52">
+    <h1 className="block font-title text-25 leading-7 sm:text-30 lg:text-42 xl:text-55.5 lg:leading-tight text-center mt-6 sm:mt-10 sm:leading-9 px-12 md:px-48 ml:px-52">
       {children}
     </h1>
   );
