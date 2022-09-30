22b59c66926453300b222e80cdfecf59bbaa64c0
thdvbr <soyoon.choi@code.berlin>
2022-09-30 16:02:05 +0200

    added margin lr on desktop

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Container({ children }) {
-  return <div className="xl:container px-3 md:px-8 ml:px-20 lg:px-28 mx-auto">{children}</div>;
+  return <div className="xl:container px-3 md:px-8 ml:px-36 mx-auto">{children}</div>;
 }
