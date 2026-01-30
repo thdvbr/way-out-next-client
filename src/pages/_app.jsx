94fbc7f52d56ba91bfc8de140cf4438608214b65
thdvbr <soyoon.choi@code.berlin>
2026-01-30 17:05:18 +0100

    fix hydration issues, wrap breakpointprovider in a client only dynamic import

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import '../../styles/index.css';
 import App from 'next/app';
-import { BreakpointProvider } from 'react-socks';
 import { AnimatePresence } from 'framer-motion';
 import { getClient } from '../utils/sanity.server';
 import {
@@ -12,9 +12,15 @@ import {
 } from '../utils/queries';
 import { AppWrapper } from '../context/state';
 
+// Dynamically import BreakpointProvider so it only runs on the client
+const BreakpointProvider = dynamic(
+  () => import('react-socks').then((mod) => mod.BreakpointProvider),
+  { ssr: false }
+);
+
 function MyApp({ Component, pageProps, router, pageData, staffData, bottomAds, sideAds }) {
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence exitBeforeEnter initial={false}>
       <AppWrapper pageData={pageData} staffData={staffData} bottomAds={bottomAds} sideAds={sideAds}>
         <BreakpointProvider>
           <Component {...pageProps} key={router.route} />
