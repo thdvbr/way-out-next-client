df9828d96a3ed6c17c3ef6e0a163bfdb8e9155af
thdvbr <soyoon.choi@code.berlin>
2026-01-30 19:57:18 +0100

    resize post title and subtitle sizes across different platform

--- src/components/post-subtitle.jsx
+++ src/components/post-subtitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function PostSubtitle({ children }) {
   return (
-    <h1 className="block font-subtitle text-22 leading-7 sm:text-27.5 sm:leading-8 text-center lg:text-43 xl:text-57 lg:leading-tight mb-9 xl:px-60 px-8 ml:px-48 md:px-44">
+    <h1 className="block font-subtitle text-22 leading-6 sm:text-27.5 sm:leading-7 text-center lg:text-40 xl:text-54 lg:leading-10 mb-9 xl:px-60 px-8 ml:px-48 md:px-44">
       {children}
     </h1>
   );
