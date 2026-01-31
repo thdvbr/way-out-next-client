d5630e12c488e7b5bcec8d4a7d9fbaff7d6ce446
thdvbr <soyoon.choi@code.berlin>
2026-01-31 13:57:28 +0100

    transpile packages config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ['cdn.sanity.io'], // keep your existing Sanity domain
   },
+
+  // Transpile third-party packages that ship uncompiled ES6 classes
+  transpilePackages: [
+    '@sanity/client',
+    'next-sanity',
+    'next-sanity-image',
+    'framer-motion',
+    'react-infinite-scroll-component',
+    'react-masonry-css'// add any other libraries causing "Cannot call a class as a function"
+  ],
 };
+
+module.exports = nextConfig;
