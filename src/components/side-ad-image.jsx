0986c072c9130e4aab19f240b281e3cce1a6d6bf
thdvbr <soyoon.choi@code.berlin>
2026-01-31 01:40:25 +0100

    use Image component instead of img

--- src/components/side-ad-image.jsx
+++ src/components/side-ad-image.jsx
@@ -4,19 +4,22 @@ import imageUrlBuilder from '@sanity/image-url';
 import { sanityClient } from '../utils/sanity.server';
 
 const SideAdImage = ({ image, url }) => {
+  if (!image) return null;
   const urlFor = (source) => {
     return imageUrlBuilder(sanityClient).image(source);
   };
 
-  const adImage = image && (
-    <img
-      className="sm:w-16 md:w-20 ml:w-24 lg:w-28 xl:w-30"
-      src={urlFor(image).width(110).url()}
-    />
-  );
   return (
     <div className="sticky top-1/3">
-      <a href={url}>{adImage}</a>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <Image
+          src={urlFor(image).width(110).url()}
+          alt={image.alt || 'Advertisement'}
+          width={110}
+          height={110} // you can adjust this or calculate aspect ratio dynamically
+          className="sm:w-16 md:w-20 ml:w-24 lg:w-28 xl:w-30"
+        />
+      </a>
     </div>
   );
 };
