67bdffde4732ec8a2c2fa87c1c7b0d8caf46e4e6
thdvbr <soyoon.choi@code.berlin>
2026-02-02 00:13:52 +0100

    style tags order them in a row

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Tags = ({ tags = [] }) => {
-  return tags.map((tag) => {
-    return <div key={tag._key}>{tag}</div>;
-  });
+  return (
+    <div className="flex flex-row gap-2">
+      {tags.map((tag) => (
+        <div
+          className="px-2 py-0 border border-white font-secondary"
+          key={tag._key}>
+          {tag}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Tags;
