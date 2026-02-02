33fe458d7e6852fc704fbef4001a8fc7a7c76166
thdvbr <soyoon.choi@code.berlin>
2026-02-02 13:58:39 +0100

    fix unique key error on tags and tracklist component

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Tags = ({ tags = [] }) => {
   return (
     <div className="flex flex-row gap-2">
-      {tags.map((tag) => (
+      {tags.map((tag, index) => (
         <div
           className="px-2 py-0 border border-white font-secondary"
-          key={tag._key}>
+          // eslint-disable-next-line react/jsx-closing-bracket-location
+          key={tag._key || `${tag}-${index}`}>
           {tag}
         </div>
       ))}
--- src/components/tracklist.jsx
+++ src/components/tracklist.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 const Tracklist = ({ tracklist = [] }) => {
-  return tracklist.map((track) => (
-    <>
-      <div className="font-main" key={track._key}>
-        {track.artist}
-      </div>
-      <div className="font-secondary" key={track._key}>
-        {track.title}
-      </div>
-    </>
+  return tracklist.map((track, index) => (
+    <div key={track._key || index}>
+      <div className="mt-2 font-main">{track.artist}</div>
+      <div className="mb-2 -mt-0.5 font-secondary text-13">{track.title}</div>
+    </div>
   ));
 };
 
