cb388e7355f0d4eebf418abde32ea10fd60ad4ff
thdvbr <soyoon.choi@code.berlin>
2026-02-07 19:04:19 +0100

    preview causes undefinied on production

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -26,21 +26,25 @@ function MyApp({
         pageData={pageData}
         staffData={staffData}
         bottomAds={bottomAds}
-        sideAds={sideAds}
-      >
+        sideAds={sideAds}>
         <Component {...pageProps} key={router.route} />
       </AppWrapper>
     </AnimatePresence>
   );
 }
-MyApp.getInitialProps = async (appContext, preview) => {
+MyApp.getInitialProps = async (appContext) => {
+  const preview = false; // default to false
   const pageProps = await App.getInitialProps(appContext);
   const pageData = await getClient(preview).fetch(pageQuery);
   const staffData = await getClient(preview).fetch(staffQuery);
   const bottomAds = await getClient(preview).fetch(bottomAdQuery);
   const sideAds = await getClient(preview).fetch(sideAdQuery);
   return {
-    ...pageProps, pageData, staffData, bottomAds, sideAds,
+    ...pageProps,
+    pageData,
+    staffData,
+    bottomAds,
+    sideAds,
   };
 };
 
