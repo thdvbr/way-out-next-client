b87141dd6a14a1f71f1bbaf45f9ec8d28c30049f
thdvbr <soyoon.choi@code.berlin>
2026-02-09 23:28:16 +0100

    change artistlink to connect here

--- src/components/artist-link.jsx
+++ src/components/artist-link.jsx
@@ -6,11 +6,11 @@ import { Content } from './index';
 const ArtistLink = ({ artistLink }) => {
   return (
     <div className="xl:text-22.5 xl:leading-7 text-center my-12 tracking-wider">
-      <p className="font-secondary font-extrabold text-16 ml:text-20 xl:text-26.5 xl:leading-8">
-        More About the Artist
+      <p className="font-secondary mb-3 font-extrabold text-16 ml:text-20 xl:text-26.5 xl:leading-8">
+        Connect here
       </p>
-      <span className="font-agrandir text-14 ml:text-18 xl:text-24 xl:leading-8">
-        <Content body={artistLink} />
+      <span className="whitespace-pre-line font-agrandir text-14 ml:text-18 xl:text-24 xl:leading-8">
+        {artistLink}
       </span>
     </div>
   );
