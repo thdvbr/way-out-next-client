75bb4b395874168d856393b494816213fa0178f9
thdvbr <soyoon.choi@code.berlin>
2026-02-19 00:54:40 +0100

    flip selection colour on dark mode

--- src/components/theme-wrapper.jsx
+++ src/components/theme-wrapper.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export default function ThemeWrapper({ children, theme = 'light' }) {
   return (
     <div
-      className={` ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}
-    >
+      className={` ${theme === 'dark' ? 'dark bg-black text-white' : 'bg-white text-black'}`}>
       {children}
     </div>
   );
