973de54116f59362f207ca8d0e621c27944bd41d
thdvbr <soyoon.choi@code.berlin>
2026-02-22 20:21:19 +0100

    add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import '../../styles/index.css';
 import App from 'next/app';
 import { AnimatePresence } from 'framer-motion';
@@ -21,16 +22,44 @@ function MyApp({
   sideAds,
 }) {
   return (
-    <AnimatePresence exitBeforeEnter initial={false}>
-      <AppWrapper
-        pageData={pageData}
-        staffData={staffData}
-        bottomAds={bottomAds}
-        sideAds={sideAds}
-      >
-        <Component {...pageProps} key={router.route} />
-      </AppWrapper>
-    </AnimatePresence>
+    <>
+      {' '}
+      <Head>
+        <link rel="icon" href="/assets/favicons/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="android-chrome-192x192"
+          sizes="192x192"
+          href="/assets/favicons/android-chrome-192x192.png"
+        />
+      </Head>
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <AppWrapper
+          pageData={pageData}
+          staffData={staffData}
+          bottomAds={bottomAds}
+          sideAds={sideAds}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AppWrapper>
+      </AnimatePresence>
+    </>
   );
 }
 MyApp.getInitialProps = async (appContext) => {
