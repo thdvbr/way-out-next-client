5ca94eadbb73a105e99bb000b7f95fcdcf626d49
thdvbr <soyoon.choi@code.berlin>
2026-02-23 18:55:35 +0100

    make mobile navbar trigger space bigger

--- src/components/navbar-mobile.jsx
+++ src/components/navbar-mobile.jsx
@@ -8,9 +8,8 @@ import { useUIContext } from '../context/ui-context';
 import { debounce } from '../utils/helpers';
 
 const NavbarMobile = ({ theme = 'light' }) => {
-  const {
-    infoIsOpen, setInfoIsOpen, searchIsOpen, setSearchIsOpen,
-  } = useUIContext();
+  const { infoIsOpen, setInfoIsOpen, searchIsOpen, setSearchIsOpen } =
+    useUIContext();
   // const [menuIsOpen, setMenuIsOpen] = useState(false);
   const [visible, setVisible] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -71,8 +70,7 @@ const NavbarMobile = ({ theme = 'light' }) => {
               animate={{ x: 0 }}
               initial={{ x: -30 }}
               transition={{ ease: 'easeOut', duration: 0.7 }}
-              className="w-full"
-            >
+              className="w-full">
               <SearchBar />
             </motion.div>
           )}
@@ -80,24 +78,20 @@ const NavbarMobile = ({ theme = 'light' }) => {
         <button
           type="button"
           onClick={handleClick}
-          className="pt-4 pb-8 lg:hidden"
-        >
+          className="pt-4 pb-8 lg:hidden">
           <span>Menu</span>
         </button>
         <div
           className={`${visible ? 'absolute' : 'hidden'} -right-4`}
-          style={{ bottom: '-13.07rem' }}
-        >
+          style={{ bottom: '-13.07rem' }}>
           {/* TODO: fix hide overflow */}
           <div
-            className={`flex flex-col pl-4 pr-8 ${isDark ? 'mobile-navbar-box-dark' : 'mobile-navbar-box'}`}
-          >
+            className={`flex flex-col ${isDark ? 'mobile-navbar-box-dark' : 'mobile-navbar-box'}`}>
             <Link
               href="/interviews"
-              className="relative block py-4"
+              className="relative block py-4 ml-4 mr-8"
               onTouchStart={() => setActiveItem('interviews')}
-              onTouchEnd={() => setActiveItem(null)}
-            >
+              onTouchEnd={() => setActiveItem(null)}>
               <span className="relative inline-block group">
                 Interviews
                 <img
@@ -110,10 +104,9 @@ const NavbarMobile = ({ theme = 'light' }) => {
 
             <Link
               href="/stuff-we-like"
-              className="relative block py-4"
+              className="relative block py-4 ml-4 mr-8"
               onTouchStart={() => setActiveItem('opinions')}
-              onTouchEnd={() => setActiveItem(null)}
-            >
+              onTouchEnd={() => setActiveItem(null)}>
               <span className="relative inline-block group">
                 Opinions
                 <img
@@ -126,10 +119,9 @@ const NavbarMobile = ({ theme = 'light' }) => {
 
             <Link
               href="/radio"
-              className="relative block py-4"
+              className="relative block py-4 ml-4 mr-8"
               onTouchStart={() => setActiveItem('radio')}
-              onTouchEnd={() => setActiveItem(null)}
-            >
+              onTouchEnd={() => setActiveItem(null)}>
               <span className="relative inline-block group">
                 Radio
                 <img
@@ -141,12 +133,11 @@ const NavbarMobile = ({ theme = 'light' }) => {
             </Link>
 
             <button
-              className="relative block w-full py-4 text-left"
+              className="relative block w-full py-4 ml-4 mr-8 text-left"
               type="button"
               onClick={() => setInfoIsOpen(!infoIsOpen)}
               onTouchStart={() => setActiveItem('info')}
-              onTouchEnd={() => setActiveItem(null)}
-            >
+              onTouchEnd={() => setActiveItem(null)}>
               <span className="relative inline-block group">
                 Info
                 <img
