383e2b2d6a43a01adcf0a034ba7f75beea82538d
thdvbr <soyoon.choi@code.berlin>
2026-03-05 17:02:36 +0100

    connect donate link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,21 +9,25 @@ export default function Header({ theme }) {
   return (
     <header>
       <div className="flex relative flex-wrap -top-10 justify-between pt-16 mb-14 font-main xl:text-22.5 lg:text-17 sm:text-17 xl:pb-6">
-        <span className="z-50 button-underline">Donate</span>
+        <a
+          href="https://buy.stripe.com/test_cNibJ1dCzfO93BY7WNfAc00 "
+          target="_blank"
+          rel="noopener noreferrer"
+          className="z-50 button-underline">
+          Donate
+        </a>
         <button
           type="button"
           onClick={() => {
             setJoinIsOpen(!joinIsOpen);
             setInfoIsOpen(false);
           }}
-          className="z-50 button-underline"
-        >
+          className="z-50 button-underline">
           Join
         </button>
         <Link
           href="/"
-          className="absolute left-0 right-0 flex justify-center top-4 sm:top-7 xl:top-8"
-        >
+          className="absolute left-0 right-0 flex justify-center top-4 sm:top-7 xl:top-8">
           <div
             className={
               theme === 'dark' ? 'logo-container-dark' : 'logo-container'
